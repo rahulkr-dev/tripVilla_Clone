7870e696aeaf35c73f5b20cca7b37ebb01d88229
shivani awasthi <shvaniawsthi001@gmail.com.com>
2022-12-16 14:32:22 +0530

    paymentpage

---
+++ Frontend/shivani/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Payment from './Components/Payment';
+
+function App() {
+  return (
+    <div className="App">
+      {/* navbar */}
+     <Payment/>
+    </div>
+  );
+}
+
+export default App;
