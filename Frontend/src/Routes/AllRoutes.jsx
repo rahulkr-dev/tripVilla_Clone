ff4f50764165e32dcb5015854b7e91c1fc911e12
Raman <rushivenketesh@gmail.com>
2022-12-18 21:48:03 +0530

    done some changes

--- Frontend/src/Routes/AllRoutes.jsx
+++ Frontend/src/Routes/AllRoutes.jsx
@@ -7,12 +7,13 @@ import SignupCard from "../Components/auth/SignupCard";
 import Home from "../Pages/Home";
 import Collections from "../Pages/Collections";
 import Hotels from "../Components/Hotelpage/Hoteldetails";
-import About from './../Pages/About';
+import About from "./../Pages/About";
 import Privacy from "../Pages/Privacy";
-import Faq from './../Pages/Faq';
-import Terms from './../Pages/Terms';
+import Faq from "./../Pages/Faq";
+import Terms from "./../Pages/Terms";
 import SingleHotelDetails from "../Pages/SingleHotelDetails";
 import Booked from "../Components/Payment/Booked";
+import { Private } from "./Private";
 
 function AllRoutes() {
   return (
@@ -24,14 +25,20 @@ function AllRoutes() {
         <Route path="/signup" element={<SignupCard />}></Route>
         <Route path="/resetpassword" element={<Forgetpassword />}></Route>
         <Route path="/collections" element={<Collections />}></Route>
-        <Route path="/about-us" element={<About/>}></Route>
-        <Route path="/privacy" element={<Privacy/>}></Route>
-        <Route path="/faq" element={<Faq/>}></Route>
-        <Route path="/terms" element={<Terms/>}></Route>
-        <Route path="/payment" element={<SingleHotelDetails/>}></Route>
-        <Route path="/hotels" element={<Hotels/>}></Route>
-        <Route path="/booked" element={<Booked/>}></Route>
-
+        <Route path="/about-us" element={<About />}></Route>
+        <Route path="/privacy" element={<Privacy />}></Route>
+        <Route path="/faq" element={<Faq />}></Route>
+        <Route path="/terms" element={<Terms />}></Route>
+        <Route
+          path="/payment"
+          element={
+            <Private>
+              <SingleHotelDetails />
+            </Private>
+          }
+        />
+        <Route path="/hotels" element={<Hotels />}></Route>
+        <Route path="/booked" element={<Booked />}></Route>
       </Routes>
     </>
   );
